da4a395b9dd69e3b8f2296c5a04eb2a513a04432
mihirhebalkar <mihirhebalkar07@gmail.com>
2024-03-04 10:21:30 +0530

    Codes added.

---
+++ Lecture 14/src/Currency.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Currency {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number : ");
+		int n1 = sc.nextInt();
+		
+		StringBuffer s1 = new StringBuffer();
+		
+		
+		
+	}
+}
