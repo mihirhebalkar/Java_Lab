bc137bfae5d13c5f8f263dd2fc30ddac7454ff28
mihirhebalkar <mihirhebalkar07@gmail.com>
2024-03-04 09:55:43 +0530

    Lecture 14 notes added.

--- Lecture 14/src/Palindrome.java
+++ Lecture 14/src/Palindrome.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 
-public class ReverseString {
+public class Palindrome {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your string : ");
-        String s = sc.nextLine();
+        String s = sc.nextLine().toLowerCase();
 
         String rs = "";
 
         for (int i = s.length() - 1; i >= 0; i--) {
             rs += s.charAt(i);
         }
-
-        System.out.println("Reversed String: " + rs);
+        
+        if(rs.equals(s)) {
+        	System.out.println(s + " is a palindrome.");
+        } else {
+        System.out.println(s + " is not a palindrome.");
+        }
     }
 }
